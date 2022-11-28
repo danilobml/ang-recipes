b7577deb746c445fdf34af3e81a3c79d3d30b496
danilobml <danbarml@gmail.com>
2022-11-28 22:53:18 +0100

    first structure for components

--- src/app/shopping-list/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list/shopping-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { Ingredient } from './ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-
+  ingredients:Ingredient[] = [
+    new Ingredient('apple', 5),
+    new Ingredient('tomato', 10)
+  ]
 }
